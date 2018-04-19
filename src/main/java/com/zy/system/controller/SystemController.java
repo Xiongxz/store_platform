dc5456892fa0d72d9c210ef7f556c2e357bc7fb0
superxxzxxz <654962327@qq.com>
2018-04-19 14:28:14 +0800

    Revert "Initial commit"
    This reverts commit 524941e249083251f7c915fa2d76ef294a845b29.

--- src/main/java/com/zy/system/controller/SystemController.java
+++
@@ -1,7 +0,0 @@
-package com.zy.system.controller;
-
-/**
- * Created by xxz on 2018/4/19 0019
- */
-public class SystemController {
-}
--- src/main/java/com/zy/test/controller/TestController.java
+++
@@ -1,8 +0,0 @@
-package com.zy.test.controller;
-
-/**
- * Created by xxz on 2018/4/19 0019
- */
-public class TestController {
-
-}
